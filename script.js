95e4b8ed86e05f4e66ff65e27bcf935139d0a39b
Yamen <osaretinosayamen@outlook.com>
2017-05-20 13:30:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,79 @@
+
+module.exports ={	
+	function room (name, size, type)
+	{
+	   this.name = name;
+	   this.type = type;
+	   this.size = size;
+	   this.isOccupied = false;
+	   
+    	 this.occupy =  function() 
+    		{
+    		     this.isOccupied = true ;
+    		     return this;
+    		};
+    	
+    	this.getStatus = function()
+	      	{
+	      	  return this.isOccupied;
+	      	};
+      	
+      	this.getName = function()
+	        {
+	          return this.name;
+	        };
+        
+      	this.getType = function()
+	        {
+	          return this.type;
+	        };
+      	this.getSize = function()
+	        {
+	          return this.size;
+	        };
+	},
+		
+	function getRooms()
+	{
+		let roomsList = [];
+		
+		let meeting1 = new room("Meeting1", 10, "meetings");
+		roomsList.push(meeting1);
+
+		let meeting2 = new room("Meeting2", 10, "meetings");
+		meeting2.occupy();
+		roomsList.push(meeting2);
+
+		let game1 = new room("Game1", 10, "games");
+		roomsList.push(game1);
+
+		let game2 = new room("Game2", 3, "games");
+		game2.occupy();
+		roomsList.push(game2);
+
+		let quite1 = new room("Quiet-Time1", 4, "quiet-time");
+		roomsList.push(quite1);
+
+		let quite2 = new room("Quiet-Time2", 2, "quiet-time");
+		quite2.occupy();
+		roomsList.push(quite2);
+
+		let working1 = new room("Working1", 6, "Working");
+		roomsList.push(working1);
+
+		let working2 = new room("Working2", 12, "Working");
+		working2.occupy();
+		roomsList.push(working2);
+
+		let learning1 = new room("Learning1", 6, "learning");
+		roomsList.push(learning1);
+
+		let learning2 = new room("Learning2", 8, "learning");
+		learning2.occupy();
+		roomsList.push(learning2);
+
+		return roomsList;
+	},
 	
 	/** 
 	*  findRoomByStatus() finds rooms for Andelans based on the Status of the room 
@@ -40,8 +116,8 @@
 
 		document.getElementById("result").innerHTML = roomsReturned;
 		return roomsReturned;
-	},
-
+	}
+}
 
 	/**
 	* findRoomByName() find rooms for Andelans based on the type of rooms.
@@ -70,77 +146,5 @@
 	* @params: anArrayContainingRooms (array of rooms), sizeOfRoom(name of room)
 	*/
 
-	function getRooms()
-	{
-		let roomsList = [];
+
 		
-		let meeting1 = new room("Meeting1", 10, "meetings");
-		roomsList.push(meeting1);
-
-		let meeting2 = new room("Meeting2", 10, "meetings");
-		meeting2.occupy();
-		roomsList.push(meeting2);
-
-		let game1 = new room("Game1", 10, "games");
-		roomsList.push(game1);
-
-		let game2 = new room("Game2", 3, "games");
-		game2.occupy();
-		roomsList.push(game2);
-
-		let quite1 = new room("Quiet-Time1", 4, "quiet-time");
-		roomsList.push(quite1);
-
-		let quite2 = new room("Quiet-Time2", 2, "quiet-time");
-		quite2.occupy();
-		roomsList.push(quite2);
-
-		let working1 = new room("Working1", 6, "Working");
-		roomsList.push(working1);
-
-		let working2 = new room("Working2", 12, "Working");
-		working2.occupy();
-		roomsList.push(working2);
-
-		let learning1 = new room("Learning1", 6, "learning");
-		roomsList.push(learning1);
-
-		let learning2 = new room("Learning2", 8, "learning");
-		learning2.occupy();
-		roomsList.push(learning2);
-
-		return roomsList;
-	},
-
-	function room (name, size, type)
-	{
-	   this.name = name;
-	   this.type = type;
-	   this.size = size;
-	   this.isOccupied = false;
-	   
-    	 this.occupy =  function() 
-    		{
-    		     this.isOccupied = true ;
-    		     return this;
-    		};
-    	
-    	this.getStatus = function()
-	      	{
-	      	  return this.isOccupied;
-	      	};
-      	
-      	this.getName = function()
-	        {
-	          return this.name;
-	        };
-        
-      	this.getType = function()
-	        {
-	          return this.type;
-	        };
-      	this.getSize = function()
-	        {
-	          return this.size;
-	        };
-	}	
